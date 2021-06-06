f62f9e92317bc865a6a013636cb1f22938971015
Manoj <manoj.w@e-zest.in>
2021-06-06 17:22:46 +0530

    - added native splash screen - change the app icon

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -31,170 +31,173 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        shrinkWrap: true,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'Send whatsapp messages without saving the number',
-                style: TextStyle(fontSize: 16),
-                textAlign: TextAlign.center,
-              ),
-              Container(
-                height: 60,
-                alignment: Alignment.center,
-                margin: const EdgeInsets.only(top: 20),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    CountryCodePicker(
-                      padding: const EdgeInsets.only(bottom: 20.0),
-                      initialSelection: 'IN',
-                      showCountryOnly: true,
-                      favorite: const ['+91', 'IN'],
-                      searchDecoration: const InputDecoration(hintText: 'Search Country'),
-                      onChanged: (code) {
-                        setState(() {
-                          _countryCode = code.dialCode.toString();
-                        });
-                      },
-                      flagDecoration: const BoxDecoration(shape: BoxShape.circle),
-                      flagWidth: 40,
-                    ),
-                    const SizedBox(
-                      width: 5.0,
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: TextField(
-                        maxLength: 10,
-                        controller: wpNumber,
-                        cursorColor: Constant.black,
-                        keyboardType: TextInputType.phone,
-                        style: const TextStyle(fontSize: 15),
-                        decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.only(top: 10.0, left: 10.0),
-                          hintText: '0123456789',
-                          // prefixIcon: Padding(
-                          //   padding: const EdgeInsets.only(left: 10.0),
-                          //   child: Icon(
-                          //     FontAwesomeIcons.whatsapp,
-                          //     color: Constant.whatsappGreen,
-                          //   ),
-                          // ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Constant.whatsappGreen, width: 2.0),
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                          border: const OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(8),
+    return GestureDetector(
+      onTap: () => Focus.of(context).unfocus(),
+      child: Scaffold(
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          shrinkWrap: true,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Send whatsapp messages without saving the number',
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+                Container(
+                  height: 60,
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.only(top: 20),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      CountryCodePicker(
+                        padding: const EdgeInsets.only(bottom: 20.0),
+                        initialSelection: 'IN',
+                        showCountryOnly: true,
+                        favorite: const ['+91', 'IN'],
+                        searchDecoration: const InputDecoration(hintText: 'Search Country'),
+                        onChanged: (code) {
+                          setState(() {
+                            _countryCode = code.dialCode.toString();
+                          });
+                        },
+                        flagDecoration: const BoxDecoration(shape: BoxShape.circle),
+                        flagWidth: 40,
+                      ),
+                      const SizedBox(
+                        width: 5.0,
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: TextField(
+                          maxLength: 10,
+                          controller: wpNumber,
+                          cursorColor: Constant.black,
+                          keyboardType: TextInputType.phone,
+                          style: const TextStyle(fontSize: 15),
+                          decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.only(top: 10.0, left: 10.0),
+                            hintText: '0123456789',
+                            // prefixIcon: Padding(
+                            //   padding: const EdgeInsets.only(left: 10.0),
+                            //   child: Icon(
+                            //     FontAwesomeIcons.whatsapp,
+                            //     color: Constant.whatsappGreen,
+                            //   ),
+                            // ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Constant.whatsappGreen, width: 2.0),
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            border: const OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(8),
+                              ),
                             ),
                           ),
                         ),
                       ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 12),
+                  alignment: Alignment.bottomCenter,
+                  child: TextField(
+                    controller: wpMessage,
+                    cursorColor: Constant.black,
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 4,
+                    style: const TextStyle(fontSize: 15),
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Constant.whatsappGreen, width: 2.0),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(8),
+                        ),
+                      ),
+                      hintText: 'Message',
                     ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Wrap(
+                  alignment: WrapAlignment.center,
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  spacing: 5.0,
+                  children: [
+                    for (int i = 0; i < CustomData.customMessages.length; i++) ...{
+                      GestureDetector(
+                        onTap: () {
+                          print(CustomData.customMessages[i]);
+                          wpMessage.text = CustomData.customMessages[i].toString();
+                        },
+                        child: Chip(
+                          label: Text(
+                            CustomData.customMessages[i],
+                            style: TextStyle(color: Constant.darkBlue),
+                          ),
+                          backgroundColor: Constant.whatsappGreen.withOpacity(0.8),
+                        ),
+                      )
+                    }
                   ],
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(top: 12),
-                alignment: Alignment.bottomCenter,
-                child: TextField(
-                  controller: wpMessage,
-                  cursorColor: Constant.black,
-                  keyboardType: TextInputType.multiline,
-                  maxLines: 4,
-                  style: const TextStyle(fontSize: 15),
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Constant.whatsappGreen, width: 2.0),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    border: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(8),
-                      ),
-                    ),
-                    hintText: 'Message',
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Wrap(
-                alignment: WrapAlignment.center,
-                crossAxisAlignment: WrapCrossAlignment.center,
-                spacing: 5.0,
-                children: [
-                  for (int i = 0; i < CustomData.customMessages.length; i++) ...{
-                    GestureDetector(
-                      onTap: () {
-                        print(CustomData.customMessages[i]);
-                        wpMessage.text = CustomData.customMessages[i].toString();
-                      },
-                      child: Chip(
-                        label: Text(
-                          CustomData.customMessages[i],
-                          style: TextStyle(color: Constant.darkBlue),
+                const SizedBox(height: 10),
+                const Divider(),
+                const SizedBox(height: 10),
+                Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.47,
+                    child: Center(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Constant.whatsappGreen,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4),
+                          ),
                         ),
-                        backgroundColor: Constant.whatsappGreen.withOpacity(0.8),
-                      ),
-                    )
-                  }
-                ],
-              ),
-              const SizedBox(height: 10),
-              const Divider(),
-              const SizedBox(height: 10),
-              Center(
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.47,
-                  child: Center(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Constant.whatsappGreen,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                      ),
-                      onPressed: () {
-                        launchWhatsApp(
-                          _countryCode,
-                          wpNumber.text.trim().toString(),
-                          wpMessage.text.toString(),
-                        );
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          SizedBox(
-                            width: 5.0,
-                          ),
-                          FaIcon(
-                            FontAwesomeIcons.whatsapp,
-                          ),
-                          SizedBox(
-                            width: 15.0,
-                          ),
-                          Expanded(
-                            child: Text(
-                              'Send Message',
+                        onPressed: () {
+                          launchWhatsApp(
+                            _countryCode,
+                            wpNumber.text.trim().toString(),
+                            wpMessage.text.toString(),
+                          );
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            SizedBox(
+                              width: 5.0,
                             ),
-                          ),
-                        ],
+                            FaIcon(
+                              FontAwesomeIcons.whatsapp,
+                            ),
+                            SizedBox(
+                              width: 15.0,
+                            ),
+                            Expanded(
+                              child: Text(
+                                'Send Message',
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              )
-            ],
-          ),
-        ],
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
